117c421597958802805807cbff622e72814b50b4
afan31 <afan.khan50@gmail.com>
2020-08-02 00:40:12 -0400

    Detect capital use of characters in a string

---
+++ DetectCapital.java
@@ -0,0 +1,33 @@
+class Solution {
+  public boolean detectCapitalUse(String word) {
+      if (word == null || word == ""){
+          return false;
+      }
+      char[] wordArray = word.toCharArray();
+      boolean firstCap = checkForUppercase(wordArray[0]);
+      int wordLength = word.length();
+      int count = 0;
+      for(int i = 1; i < wordLength; i++){
+          char a = wordArray[i];
+          boolean isUppercase = checkForUppercase(a);
+          if (isUppercase){
+              count++;
+          }
+      }
+      if (count == 0 || (count == wordLength-1 && firstCap)){
+          return true;
+      }else if (firstCap && count == 0){ 
+          return true;
+      }
+      return false;
+  }
+  
+  boolean checkForUppercase(char a){
+      int asciiVal = (int)a;
+      // check for ascii range of capital characters
+      if (asciiVal >= 65 && asciiVal <= 90){
+          return true;
+      }
+      return false;
+  }
+}
